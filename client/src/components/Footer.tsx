ad8e55e34920436e1ca075091af086ce3daa0aee
TKU-AI-411630188 <112963598+Alison411630188@users.noreply.github.com>
2026-04-17 03:03:58 +0000

    7877

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -62,7 +62,7 @@ export default function Footer() {
           <h4 className="font-semibold mb-4 text-slate-200">官方連結</h4>
           <ul className="space-y-2 text-sm text-slate-400">
             <li>
-              <a href="https://www.microsoft.com/zh-tw/microsoft-365/sharepoint/collaboration" target="_blank" rel="noopener noreferrer" className="hover:text-primary transition">
+              <a href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=4765445b-32c6-49b0-83e6-1d93765276ca&redirect_uri=https%3A%2F%2Fm365.cloud.microsoft%2Flandingv2&response_type=code%20id_token&scope=openid%20profile%20https%3A%2F%2Fwww.office.com%2Fv2%2FOfficeHome.All&response_mode=form_post&nonce=639119917446815896.NWZlMzE1ZDktYzUyMC00M2I1LTk2M2EtNWU2NTllMmMxZmIxMGJjNDcwZTctMDA4NC00YTQ4LTk1ZDctYmY0N2FiOWNhNWFm&ui_locales=zh-TW&mkt=zh-TW&prompt=select_account&client-request-id=020a395b-e9a8-4d6c-8c74-feb8f40230f7&state=4fNVzUvnqaXKGdvGwPIecCNAdmAOc1UlbrR7Rst9mRfoAUymeXuNWZhWkVPanxF4h4gk-Fg_JyIYTZHV9gYbA09SRHPMdPC3EilO2zFPE6W0EKt6QHtkCIVj3V9BCn0sJcL82yw-oSb6C0nL4b-X_IH5xeDvP9FvVmT4p18yju1-d-9D91S9eZNdSVBR6_U_fVzdIIUG9_-Qt9vJWeMN4-1KJsnycHX7R--oIUrt39rrZj2N0jW7h3ENgjEUuiY0XxmjGgLJ9AYqbzqAycwlIMCp_-c-thuVpyxB2KZ4L6YxuDs24qKHkhUwX5bR0nkKzkqmQjAeD-QLmQ9WABsdL96N_XkoTxMXbE779KkDJ6MwES5AKLyyeofmIDOA5W5UhyzvW2D39GiY1BPTRw4cCh_m2G2cnBbnHHNuudI6SHmTxJmkghJVHOqF5OSUuO9z7UFerf9LIoq9g0ery3FIMFxjUfdOvyG78MozDs0VaOD3kfaB2qTnIqLFmfii7M-jSogi6UyeTdR2wurN6v3Ezw&x-client-SKU=ID_NET8_0&x-client-ver=8.14.0.0">
                 SharePoint 網頁版
               </a>
             </li>
